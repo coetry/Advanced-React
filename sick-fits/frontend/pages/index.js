5dc7c8d82694fa406ff9b20359d98b7a0d24206c
Allen Hai <allenhai03@gmail.com>
2018-11-06 13:44:31 -0600

    add index and sell pages with routes between them

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+const Home = props => <div>
+  <p>yo</p>
+  <Link href='/sell'><a>sell</a></Link>
+</div>
+ 
+export default Home
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,6 @@
+import Link from 'next/link'
+const Sell = props => <div>
+  <p>sell</p>
+  <Link href='/'><a>home</a></Link>
+</div>
+export default Sell
